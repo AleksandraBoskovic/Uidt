5f9f41532b9fef611ebe4f2238eb9fc1bd583c77
Aleksandra Boskovic <38027665+AleksandraBoskovic@users.noreply.github.com>
2020-09-27 00:12:59 +0200

    Update Nejednacina.thy

--- Nejednacina.thy
+++ Nejednacina.thy
@@ -52,12 +52,13 @@ lemma poredjenje_u_imeniocu [simp]:
   assumes "b > a"
   shows "1/b < 1/a"
   by (simp add: assms(2) assms(3) frac_less2)
-  
+
+
 lemma stepenovan_pozitivan [simp]:
    fixes a :: real and n::nat
-   assumes "a ≥ 0"
-   assumes "a ≤ 1"
-   shows "a ^ n ≥ 0"
+   assumes "a > 0"
+   assumes "a < 1"
+   shows "a ^ n > 0"
    using assms
    apply (induction n)
     apply auto
@@ -79,19 +80,47 @@ lemma zbir_pozitivan [simp]:
   using assms
   by auto
 
+lemma pomocna [simp]:
+fixes a b c:: real
+  assumes "b < c"
+  assumes "a > 0"
+  assumes "b > 0"
+  assumes "c > 0"
+  shows "a*b < a*c"
+  using assms
+  by auto
+
+lemma pomocna2 [simp]:
+  fixes a:: real
+  assumes "a > 0"
+  shows "1 / a > 0"
+  using assms
+  by auto
 
 lemma moj_seminarski:
+
   fixes a b c :: real
   assumes "a+b+c=1" : zbir
   assumes "a > 0" 
   assumes "b > 0" 
   assumes "c > 0" 
-  shows "1 + 8/27*a*b*c ≥ a/(a^3 + b^2*c + c^2*b) + b/(b^3 + c^2*a + a^2*c) + c/(c^3 + a^2*b + b^2*a) "
+  shows " a/(a^3 + b^2*c + c^2*b) + b/(b^3 + c^2*a + a^2*c) + c/(c^3 + a^2*b + b^2*a) ≤ 1 + 8/27*a*b*c "
   using assms
 proof-
-  have "a/(a^3 + b^2*c + c^2*b) + b/(b^3 + c^2*a + a^2*c) + c/(c^3 + a^2*b + b^2*a)<a/(a^3) + b/(b^3) + c/(c^3)"
-    by auto
-
+  from `c>0 `have "c^2 > 0" by auto
+  from this and `b>0 ` have "c^2*b > 0" by auto
+  from  `b>0 ` and  `c>0 ` have "b^2*c > 0" by auto
+  from `c^2*b > 0` and `b^2*c > 0` have "b^2*c + c^2*b >0" by auto
+  from `a>0 `have "a^3 > 0" by auto
+  from this and `b^2*c + c^2*b >0` have "a^3 + (b^2*c + c^2*b) > a^3" by auto
+  from `a^3 > 0` and `b^2*c + c^2*b >0` have "a^3 + (b^2*c + c^2*b) > 0" by auto
+  from `a^3 > 0` and `a^3 + (b^2*c + c^2*b) > 0` and `a^3 + (b^2*c + c^2*b) > a^3`
+  have "1/(a^3 + (b^2*c + c^2*b)) < 1/a^3" by auto
+  from `a^3 + (b^2*c + c^2*b) > 0` have  "1/(a^3 + (b^2*c + c^2*b)) > 0" by auto
+  from `a^3 > 0` have "1/a^3 > 0" by auto
+  from `1/(a^3 + (b^2*c + c^2*b)) < 1/a^3` and `1/(a^3 + (b^2*c + c^2*b)) > 0` and `1/a^3 > 0` and `a>0`
+  have "a * (1/(a^3 + (b^2*c + c^2*b))) < a * (1/a^3)" by auto
+  sorry
 qed
 
 
